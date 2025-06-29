72a0da49b5a6950a7567f6b48ea6337e482d9dc7
Damaris Lara <tu-correo-de-github@example.com>
2025-06-29 16:44:12 -0500

    primera etapa

---
+++ Tarea semana #5/tareaSemana#5.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+
+namespace CursoAsignaturas
+{
+    // Clase que representa un curso con asignaturas
+    class Curso
+    {
+        // Propiedad: lista de asignaturas
+        public List<string> Asignaturas { get; set; }
