be95b8d6db49a14061165775615fa1d134d8cd56
Damaris Lara <tu-correo-de-github@example.com>
2025-06-29 18:09:28 -0500

    Cuadrado

--- Semana#2/ejercicioCuadrado.cs
+++ Semana#2/ejercicioCuadrado.cs
@@ -3,12 +3,25 @@ using System;
 // La clase Cuadrado representa un cuadrado con un lado
 public class Cuadrado
 {
+    // Campo privado que representa el lado del cuadrado
     private double lado;
-     public Cuadrado(double lado)
+
+    // Constructor que inicializa el lado del cuadrado
+    public Cuadrado(double lado)
     {
         this.lado = lado;
     }
+
+    // CalcularArea es una función que devuelve un valor double,
+    // se utiliza para calcular el área de un cuadrado
     public double CalcularArea()
     {
         return lado * lado;
     }
+
+    // CalcularPerimetro devuelve el perímetro del cuadrado
+    public double CalcularPerimetro()
+    {
+        return 4 * lado;
+    }
+}
