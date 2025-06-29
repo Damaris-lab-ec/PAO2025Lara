75950f00ae24ff0c2cb5523d0e11e6aee57552ea
Damaris Lara <tu-correo-de-github@example.com>
2025-06-29 17:18:39 -0500

    clase de asignatura

--- Tarea semana #5/ejercicio#3.cs
+++ Tarea semana #5/ejercicio#3.cs
@@ -1,2 +1,18 @@
 using System;
 using System.Collections.Generic;
+namespace NotasAsignaturas
+{
+    // Clase que representa una asignatura con su nota
+    class Asignatura
+    {
+        public string Nombre { get; set; }
+        public double Nota { get; set; }
+
+        public Asignatura(string nombre)
+        {
+            Nombre = nombre;
+        }
+    }
+
+    class Curso
+    {
