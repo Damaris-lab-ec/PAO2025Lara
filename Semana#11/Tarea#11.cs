1fce0fa40da637436405bddddfb89cc267c62f22
Damaris Lara <tu-correo-de-github@example.com>
2025-08-28 17:56:49 -0500

    palabras ingles y español

--- Semana#11/Tarea#11.cs
+++ Semana#11/Tarea#11.cs
@@ -11,3 +11,56 @@ class Program
         InicializarDiccionario();
         MostrarMenu();
     }
+
+    static void InicializarDiccionario()
+    {
+        // Palabras base sugeridas
+        diccionario.Add("time", "tiempo");
+        diccionario.Add("person", "persona");
+        diccionario.Add("year", "año");
+        diccionario.Add("way", "camino/forma");
+        diccionario.Add("day", "día");
+        diccionario.Add("thing", "cosa");
+        diccionario.Add("man", "hombre");
+        diccionario.Add("world", "mundo");
+        diccionario.Add("life", "vida");
+        diccionario.Add("hand", "mano");
+        diccionario.Add("part", "parte");
+        diccionario.Add("child", "niño/a");
+        diccionario.Add("eye", "ojo");
+        diccionario.Add("woman", "mujer");
+        diccionario.Add("place", "lugar");
+        diccionario.Add("work", "trabajo");
+        diccionario.Add("week", "semana");
+        diccionario.Add("case", "caso");
+        diccionario.Add("point", "punto/tema");
+        diccionario.Add("government", "gobierno");
+        diccionario.Add("company", "empresa/compañía");
+    }
+
+    static void InicializarDiccionario()
+    {
+        // Palabras base sugeridas
+        diccionario.Add("time", "tiempo");
+        diccionario.Add("person", "persona");
+        diccionario.Add("year", "año");
+        diccionario.Add("way", "camino/forma");
+        diccionario.Add("day", "día");
+        diccionario.Add("thing", "cosa");
+        diccionario.Add("man", "hombre");
+        diccionario.Add("world", "mundo");
+        diccionario.Add("life", "vida");
+        diccionario.Add("hand", "mano");
+        diccionario.Add("part", "parte");
+        diccionario.Add("child", "niño/a");
+        diccionario.Add("eye", "ojo");
+        diccionario.Add("woman", "mujer");
+        diccionario.Add("place", "lugar");
+        diccionario.Add("work", "trabajo");
+        diccionario.Add("week", "semana");
+        diccionario.Add("case", "caso");
+        diccionario.Add("point", "punto/tema");
+        diccionario.Add("government", "gobierno");
+        diccionario.Add("company", "empresa/compañía");
+    }
+
